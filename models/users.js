c7a3e88f92d413032a68e8d20374a38ace5c6aea
Palona <lainedaridan.as@gmail.com>
2023-06-01 13:44:53 +0200

    avant le deployement

--- models/users.js
+++ models/users.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
     username: String,
     password: String,
+    token: String,
+    canBookmark: Boolean,
 });
 
 const User = mongoose.model('users', userSchema);
+
 module.exports = User;
